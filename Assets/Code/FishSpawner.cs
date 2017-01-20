89a5ec94f6229ea756fd8ec735720a61ad573e21
Michal 'spolsh' Kłoś <michal.m.klos@gmail.com>
2017-01-20 23:50:22 +0100

    cat spawner

---
+++ Assets/Code/FishSpawner.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishSpawner : MonoBehaviour
+{
+
+    public float SpawnInterval = 1.0f; // s
+
+    public Vector2 SpawnArea = new Vector2(2.0f, 2.0f);
+
+    public List<GameObject> Items;
+
+    public List<GameObject> SpawnedItems;
+
+
+    void Start()
+    {
+        InvokeRepeating("SpawnItem", SpawnInterval, SpawnInterval);
+    }
+
+    void Update()
+    {
+        // if (Input.GetKeyUp(KeyCode.Space))
+        // {
+        //     Debug.Log("Poland can into space");
+        // }
+    }
+
+    void SpawnItem()
+    {
+        GameObject ItemPrefab = Items[Random.Range(0, Items.Count)];
+        Vector3 SpawnPosition = new Vector3(
+            (Random.value * 2.0f - 0.5f) * SpawnArea.x,
+            (Random.value * 2.0f - 0.5f) * SpawnArea.y,
+            0.0f
+        );
+
+        GameObject Item = Instantiate(ItemPrefab, SpawnPosition, Quaternion.identity) as GameObject;
+        SpawnedItems.Add(Item);
+
+        Debug.Log("called " + SpawnPosition);
+    }
+}
