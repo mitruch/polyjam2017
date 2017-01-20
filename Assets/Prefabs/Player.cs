cce143deea91e27e02e2e251b5bc35640e61a934
Unknown <kasiuniaczek11@interia.pl>
2017-01-20 23:58:28 +0100

    kot skacze i sie zabija o kota

---
+++ Assets/Prefabs/Player.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public Vector2 jumpForce = new Vector2(0, 300);
+    public new Rigidbody2D rigidbody2D;
+
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyUp("space")) {
+
+            rigidbody2D.velocity = Vector2.zero;
+            rigidbody2D.AddForce(jumpForce);
+        }
+        
+	}
+
+    void Die()
+    {
+        Application.LoadLevel(Application.loadedLevel);
+    }
+
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        Die();
+    }
+
+   
+
+
+}
