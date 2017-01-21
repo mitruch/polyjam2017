1231cc42a7ca139fe7a283eb35fb8f40c0342b4d
adam-andrzejczak <archound@gmail.com>
2017-01-21 15:03:22 +0100

    xxx
    xxx

---
+++ Assets/Kotek.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kotek : MonoBehaviour
+{
+    public float speed = 6.0F;
+    public float jumpSpeed = 8.0F;
+    public float gravity = 20.0F;
+    private Vector3 moveDirection = Vector3.zero;
+
+    private Rigidbody2D kotek;
+
+    // Use this for initialization
+    void Start()
+    {
+        kotek = GetComponent<Rigidbody2D>();
+        InvokeRepeating("Loguj", 1.0f, 1.0f);
+    }
+
+    void Update()
+    {
+        //Debug.Log("adsasdasd");
+        /*
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+            if (Input.GetButton("Jump"))
+            {
+                Debug.Log("Mejt jest glupi");
+
+                moveDirection.y = jumpSpeed;
+            }
+            */
+       // Debug.Log("is not grounded");
+
+        //moveDirection.y -= gravity * Time.deltaTime;
+        //kotek.velocity = new Vector2(1.0f, 1.0f);
+        Debug.Log("adsasdasd");
+    }
+
+    void Loguj()
+    {
+    }
+}
---
+++ Assets/MovingWaves.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingWaves : MonoBehaviour {
+
+    private Rigidbody2D falka;
+    private float startSeed;
+    private float intervalSeed;
+
+    // Use this for initialization
+    void Start () {
+        startSeed = Random.Range(0.0f, 10.0f);
+        intervalSeed = Random.Range(1.0f, 2.0f);
+        falka = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        falka.transform.rotation = Quaternion.Euler(0.0f, 0.0f, Mathf.Sin(intervalSeed*Time.time+ startSeed) *10);
+        //falka.transform.RotateAroundLocal(new Vector3(1.0f, 0.0f, 0.0f), 30.0f);
+        transform.RotateAround(Vector3.zero, Vector3.forward, Mathf.Sin(intervalSeed * Time.time + startSeed) * 10);
+        Debug.Log("test");
+	}
+}
