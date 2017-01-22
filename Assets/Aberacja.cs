5fc44791ad2388fb5fb382ca6f9a66447de2b19a
adam-andrzejczak <archound@gmail.com>
2017-01-22 14:02:06 +0100

    asd
    asdasd

---
+++ Assets/Aberacja.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CinematicEffects;
+
+public class Aberacja : MonoBehaviour {
+
+    private LensAberrations aberacja;
+    // Use this for initialization
+    void Start () {
+        aberacja = GetComponent<LensAberrations>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        aberacja.chromaticAberration.amount = Mathf.Min(Mathf.Pow(Time.timeScale,5), 100.0f)*Mathf.Sin(Time.time);
+	}
+}
