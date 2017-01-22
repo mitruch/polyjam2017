1a206cc46f8e36d839bae2c7a1a10a68dca95ae4
Unknown <kasiuniaczek11@interia.pl>
2017-01-22 02:43:14 +0100

    costam

---
+++ Assets/level_load.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class level_load : MonoBehaviour {
+    
+    public void loading_map()
+    {
+        Application.LoadLevel(1);
+
+    }
+
+
+
+}
